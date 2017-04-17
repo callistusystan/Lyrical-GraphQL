784501033526c2ce7fa1a42309d8a634450c11d9
callistusystan <callistusystan@gmail.com>
2017-04-17 18:49:48 +1000

    took screenshots

--- client/components/App.js
+++ client/components/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export default ({ children }) => {
-  return <div className="container">{children}</div>;
+  return (
+    <div className="container">
+      <nav>
+        <div className="nav-wrapper teal">
+          <a className="brand-logo center">Lyrical GraphQL</a>
+        </div>
+      </nav>
+      {children}
+    </div>
+  );
 };
